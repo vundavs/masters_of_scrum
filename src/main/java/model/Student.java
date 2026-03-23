268c624df59128747b0504b1cd490182691f8449
absarshayaan <absarshayaan@gmail.com>
2026-03-23 14:58:50 +0000

    implemented User and Student classes

---
+++ src/main/java/model/Student.java
@@ -0,0 +1,84 @@
+package model;
+import java.util.ArrayList;
+
+/**
+ * Represents a student user in the system.
+ * Students can search for performances, book performances, and manage their bookings.
+ */
+public class Student extends User {
+    private String name;
+    private int phoneNumber;
+    private ArrayList<Booking> bookings;
+    // TODO: add Student Preferences once implemented
+
+    /**
+     * Creates a new Student with the given details.
+     *
+     * @param email the student's email address
+     * @param password the student's password
+     * @param name the student's name
+     * @param phoneNumber the student's phone number
+     */
+    public Student(String email, String password, String name, int phoneNumber) {
+        super(email, password);
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.bookings = new ArrayList<Booking>();
+    }
+
+    /**
+     * Gets the student's name.
+     *
+     * @return the student's name
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Sets the student's name.
+     *
+     * @param name the new name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets the student's phone number.
+     *
+     * @return the student's phone number
+     */
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * Sets the student's phone number.
+     *
+     * @param phoneNumber the new phone number
+     */
+    public void setPhoneNumber(int phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+
+
+    /**
+     * Gets the student's list of bookings.
+     *
+     * @return the list of bookings
+     */
+    public ArrayList<Booking> getBookings() {
+        return bookings;
+    }
+
+    /**
+     * Adds a booking to the student's list of bookings.
+     *
+     * @param booking the booking to add
+     */
+    public void addBooking(Booking booking) {
+        this.bookings.add(booking);
+    }
+}
