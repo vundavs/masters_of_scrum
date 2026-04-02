1b4ab9aa340cb15fdd43eb8e1a16ecb4c2db6426
kmheasley <katie_caladh@icloud.com>
2026-04-02 01:56:08 +0100

    edit perfomance

--- src/main/java/model/Performance.java
+++ src/main/java/model/Performance.java
@@ -1,5 +1,6 @@
 package model;
 import java.time.LocalDateTime;
+import java.util.Collection;
 
 public class Performance {
     private long performanceID;
@@ -15,38 +16,202 @@ public class Performance {
     private double ticketPrice;
     private boolean isSponsored;
     private double sponsoredAmount;
-    private Collection<int> reviewRatings;
+    private Collection<Integer> reviewRatings;
     private Collection<String> reviewComments;
     private PerformanceStatus status;
 
-    public long getPerformanceID(Performance perfomance){
-        return performanceID
+    /**
+     * Returns the unique performance ID.
+     *
+     * @return performance ID
+     */
+    public long getPerformanceID(){
+        return performanceID;
     }
 
+    /**
+     * Returns the start date and time for a performance.
+     *
+     * @return the start date and start time
+     */
+    public LocalDateTime getStartDateTime() {
+        return startDateTime;
+    }
+
+    /**
+     * Returns the end date and time for a performance.
+     *
+     * @return the end date and ens time
+     */
+    public LocalDateTime getEndDateTime() {
+        return endDateTime;
+    }
+
+    /**
+     * Returns the names of all performers.
+     *
+     * @return performers names
+     */
+    public Collection<String> getPerformerNames() {
+        return performerNames;
+    }
+
+    /**
+     * Returns the address of the venue.
+     *
+     * @return the venue's address
+     */
+    public String getVenueAddress() {
+        return venueAddress;
+    }
+
+    /**
+     * Returns capacity of the venue.
+     *
+     * @return venue capacity
+     */
+    public int getVenueCapacity() {
+        return venueCapacity;
+    }
+
+    /**
+     * Returns whether or not the venue is outdoors.
+     *
+     * @return if the venue is outdoors or not
+     */
+    public boolean isVenueIsOutdoors() {
+        return venueIsOutdoors;
+    }
+
+    /**
+     * Returns whether or not the venue allows smoking.
+     *
+     * @return if venue allows smoking
+     */
+    public boolean doesVenueAllowsSmoking() {
+        return venueAllowsSmoking;
+    }
+
+    /**
+     * Returns the number of total tickets for performance.
+     *
+     * @return total number of tickets
+     */
+    public int getNumTicketsTotal() {
+        return numTicketsTotal;
+    }
+
+    /**
+     * Returns the number of tickets sold.
+     *
+     * @return the number of tickets sold
+     */
+    public int getNumTicketsSold() {
+        return numTicketsSold;
+    }
+
+    /**
+     * Returns the original price of ticket.
+     *
+     * @return orginal ticket price
+     */
+    public double getTicketPrice() {
+        return ticketPrice;
+    }
+
+    /**
+     * Returns whether or not the performance is sponsored.
+     *
+     * @return if the
+     */
+    public boolean getIsSponsored() {
+        return isSponsored;
+    }
+
+    /**
+     * Returns the amount a performance is sponsored by.
+     *
+     * @return sponsored amount
+     */
+    public double getSponsoredAmount() {
+        return sponsoredAmount;
+    }
+
+    /**
+     * Returns all the review ratings.
+     *
+     * @return all review ratings
+     */
+    public Collection<Integer> getReviewRatings() {
+        return reviewRatings;
+    }
+
+    /**
+     * Returns all the review comments.
+     *
+     * @return all review comments
+     */
+    public Collection<String> getReviewComments() {
+        return reviewComments;
+    }
+
+    /**
+     * Returns the status of the performance.
+     *
+     * @return performance status
+     */
+    public PerformanceStatus getStatus() {
+        return status;
+    }
+
+    /**
+     * Cancels this performance and changes status
+     */
     public void cancel(){
-        cancelByProvider();
+        this.status = PerformanceStatus.CANCELLED;
     }
 
+
+    /**
+     * checks if the event is ticketed
+     *
+     * @return whether the event is ticketed
+     */
     public boolean checkIfEventIsTicketed(){
         return event.isTicketed();
     }
 
+    /**
+     * checks if there is enough tickets left to buy
+     * @param numTicketsToBuy   the number of tickets the student wants to buy
+     * @return if there is enough tickets (t/f)
+     */
     public boolean checkIfTicketsLeft(int numTicketsToBuy){
+        assert numTicketsToBuy > 0 : "Number of tickets to buy must be greater than 0";
         int ticketsLeft;
-        ticketsLeft = TicketsTotal() - TicketsSold() - numTicketsToBuy;
+        ticketsLeft = getNumTicketsTotal() - getNumTicketsSold() - numTicketsToBuy;
         if (ticketsLeft >= 0){
             return true;
         }
         return false;
     }
 
+    /**
+     * gets the 'true'/final ticket price taking sponsors into account
+     *
+     * @return the final ticket price
+     */
     public double getFinalTicketPrice(){
-        if (isSponsored){
-            return (ticketPrice - sponsoredAmount);
+        if (getIsSponsored()) {
+            return (getTicketPrice() - getSponsoredAmount());
         }
-        return ticketPrice;
+        return getTicketPrice();
     }
 
+    /**
+     *
+     * @return
+     */
     public String getOrganiserEmail(){
         return organiser.getUser().getEmail();
                 //DK IF MATCHES - organiser?
@@ -61,6 +226,7 @@ public class Performance {
     }
 
     public boolean checkCreatedByEP(String email){
+        //
         for (EntertainmentProvider e : entertainmentProvider){
             if (e.getUser().getEmail().equals(email)){
                 //if email belongs to an EP
@@ -74,6 +240,7 @@ public class Performance {
         if (numTicketsTotal > 0){
             return true;
         }
+        return false;
     }
 
     public String getBookingDetailsForRefund(){
@@ -81,6 +248,8 @@ public class Performance {
         return getBookingDetailsRefund();
     }
 
+    //check if already sponsored
+    //JUSTIFY: a performance can only be sponsored once
     public void sponsor(double amount){
         if (sponsoredAmount > ticketPrice){
             System.out.println("Sponsored amount is greater than ticket price");
